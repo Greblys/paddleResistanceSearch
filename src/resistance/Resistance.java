8b902f7cb283ec8d1423a58336c1afb21bb8cd83
Lukas Greblikas <l.greblikas@cairnsolutions.com>
2016-07-19 16:45:03 +0100

    added Learn interface implementation

--- src/resistance/Resistance.java
+++ src/resistance/Resistance.java
@@ -1,78 +1,50 @@
 package resistance;
 
-import java.util.List;
 import java.util.Random;
+
+import javax.swing.plaf.basic.BasicProgressBarUI;
 
 import gnu.trove.map.hash.THashMap;
 
+import org.chocosolver.solver.ICause;
 import org.chocosolver.solver.ResolutionPolicy;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.IntConstraintFactory;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.explanations.RuleStore;
-import org.chocosolver.solver.search.limits.BacktrackCounter;
-import org.chocosolver.solver.search.limits.FailCounter;
 import org.chocosolver.solver.search.loop.Learn;
-import org.chocosolver.solver.search.loop.Move;
-import org.chocosolver.solver.search.loop.Propagate;
+import org.chocosolver.solver.search.loop.MoveBinaryDFS;
 import org.chocosolver.solver.search.loop.PropagateBasic;
-import org.chocosolver.solver.search.loop.SearchLoopFactory;
-import org.chocosolver.solver.search.loop.lns.LNSFactory;
 import org.chocosolver.solver.search.solution.BestSolutionsRecorder;
 import org.chocosolver.solver.search.solution.Solution;
 import org.chocosolver.solver.search.strategy.IntStrategyFactory;
-import org.chocosolver.solver.search.strategy.decision.Decision;
-import org.chocosolver.solver.search.strategy.selectors.VariableSelector;
+import org.chocosolver.solver.search.strategy.decision.IntDecision;
 import org.chocosolver.solver.search.strategy.selectors.values.IntDomainMin;
-import org.chocosolver.solver.search.strategy.strategy.AbstractStrategy;
+import org.chocosolver.solver.search.strategy.selectors.values.IntDomainRandomBound;
+import org.chocosolver.solver.search.strategy.selectors.variables.DomOverWDeg;
 import org.chocosolver.solver.trace.Chatterbox;
 import org.chocosolver.solver.trace.IMessage;
 import org.chocosolver.solver.variables.IVariableMonitor;
 import org.chocosolver.solver.variables.IntVar;
-import org.chocosolver.solver.variables.VF;
-import org.chocosolver.solver.variables.Variable;
 import org.chocosolver.solver.variables.VariableFactory;
 import org.chocosolver.solver.variables.events.IEventType;
 import org.chocosolver.solver.variables.events.IntEventType;
 import org.chocosolver.solver.search.loop.SearchLoop;
 
 @SuppressWarnings("serial")
-class Monitor implements IVariableMonitor<IntVar>{
-
-	@Override
-	public boolean why(RuleStore arg0, IntVar arg1, IEventType arg2, int arg3) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void duplicate(Solver arg0, THashMap<Object, Object> arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onUpdate(IntVar x, IEventType t)
-			throws ContradictionException {
-		// TODO Auto-generated method stub
-		System.out.println(x);
-	}
-	
-}
-
-
-@SuppressWarnings("serial")
-public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableSelector<IntVar>{
+public class Resistance implements IVariableMonitor<IntVar>, IMessage, Learn, ICause{
 	
 	static Solver s = new Solver();
-	IntVar[] R = VariableFactory.boundedArray("R", 4, 10, 1000000, s);
-	IntVar r = VariableFactory.bounded("r", 10, 1000000, s);
+	IntVar[] R = VariableFactory.boundedArray("R", 4, 1, 100, s);
+	IntVar r = VariableFactory.bounded("r", 1, 100, s);
 	IntVar maxr = VariableFactory.fixed(VariableFactory.MAX_INT_BOUND, s);
 	IntVar maxV = VariableFactory.fixed(1024, s);
 	Node[] states = new Node[16];	
 	IntVar minDiff = VariableFactory.integer("minDiff", 0, 1024, s);
 	IntVar[] diffs = VariableFactory.integerArray("global diffs", 11, 0, 1024, s);
 	int[][] map = new int[11][2];
+	IntVar[] searchVars = {R[0], R[1], R[2], R[3], r};
+	int mini = 0; //store minimum difference index
 	
 	
 	Node head = null;
@@ -115,6 +87,8 @@ public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableS
 				s.post(IntConstraintFactory.times(maxV, r, a));
 				s.post(IntConstraintFactory.sum(b,c));
 				s.post(IntConstraintFactory.eucl_div(a, c, n.getVoltage()));				
+			} else {
+				s.post(IntConstraintFactory.arithm(states[i].getVoltage(), "=", 0));
 			}
 		}
 		
@@ -161,16 +135,18 @@ public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableS
 	public void solve(){
 		
 		Chatterbox.showSolutions(s, this);
-		IntVar[] vars = {R[0], R[1], R[2], R[3], r};
 		Random generator = new Random();
-		/*
-		minDiff.addMonitor(this);
-		for(IntVar d : diffs)
-			d.addMonitor(this);
-		*/
-		//s.set(IntStrategyFactory.domOverWDeg(vars, generator.nextLong()));
+		
+		//minDiff.addMonitor(this);
+		//R[1].addMonitor(this);
+		
+		//r.addMonitor(this);
+		s.set(IntStrategyFactory.domOverWDeg(searchVars, generator.nextLong(), new IntDomainRandomBound(generator.nextLong())));
 		s.set(new BestSolutionsRecorder(minDiff));
-		s.set(IntStrategyFactory.custom(this, IntStrategyFactory.min_value_selector(), vars));
+		//DomOverWDeg dow = new DomOverWDeg(searchVars, generator.nextLong(), new IntDomainMin());
+		//SearchLoop sl = new SearchLoop(s, new PropagateBasic(), this, new MoveBinaryDFS(dow));
+		//s.set(sl);
+		//s.set(IntStrategyFactory.custom(this, IntStrategyFactory.min_value_selector(), vars));
 		//LNSFactory.rlns(s, vars, 1000, generator.nextLong(), new BacktrackCounter(s, 1000000));
 		s.findOptimalSolution(ResolutionPolicy.MAXIMIZE, minDiff);
 		System.out.println(s.isFeasible());
@@ -178,7 +154,7 @@ public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableS
 		
 		for(Solution sol : s.getSolutionRecorder().getSolutions()){
 			System.out.println(
-					String.format("Holy grail: %d %d %d %d %d", 
+					String.format("%d %d %d %d %d", 
 					sol.getIntVal(R[0]),
 					sol.getIntVal(R[1]),
 					sol.getIntVal(R[2]),
@@ -205,8 +181,8 @@ public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableS
 	}
 
 	@Override
-	public boolean why(RuleStore arg0, IntVar arg1, IEventType arg2, int arg3) {
-		// TODO Auto-generated method stub
+	public boolean why(RuleStore rs, IntVar v, IEventType et, int val) {
+		
 		return false;
 	}
 
@@ -239,41 +215,43 @@ public class Resistance implements IVariableMonitor<IntVar>, IMessage, VariableS
 	}
 
 	@Override
-	public IntVar getVariable(IntVar[] vars) {
-		boolean isInstantiated = true;
-		for(IntVar diff : diffs) {
-			if(!diff.isInstantiated()){
-				isInstantiated = false;
-				break;
+	public void forget(SearchLoop sl) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void record(SearchLoop sl) {
+		IntDecision d = (IntDecision) sl.getLastDecision();
+		IntVar v = d.getDecisionVariables();
+		System.out.println(mini);
+		System.out.println(v.getUB());
+		if(diffs[mini].getValue() != minDiff.getValue()){
+			int min = minDiff.getValue();
+			int i = 0;
+			for(;i < diffs.length; i++){
+				if(min == diffs[i].getValue())
+					break;
 			}
+			mini = i;
 		}
-			
-		if(minDiff.isInstantiated() && isInstantiated){
-			
-			for(IntVar var : vars)
-				if(!var.isInstantiated()){
-					int v = minDiff.getValue();
-					int i = 0;
-					for(;i < diffs.length; i++){
-						if(v == diffs[i].getValue())
-							break;
-					}
-					int s1 = map[i][0];
-					int s2 = map[i][1];
-					if(var == r
-							|| var == R[0] && (states[s1].isMorningIntact() || states[s2].isMorningIntact())
-							|| var == R[1] && (states[s1].isNoonIntact() || states[s2].isNoonIntact())
-							|| var == R[2] && (states[s1].isAfternoonIntact() || states[s2].isAfternoonIntact())
-							|| var == R[3] && (states[s1].isEveningIntact() || states[s2].isEveningIntact())){
-						System.out.println("Labas");
-						return var;
-					} 
-				}
-		}
+		
+		if (mini < 11){
+			int s1 = map[mini][0];
+			int s2 = map[mini][1];
+			if(v == r
+					|| v == R[0] && (states[s1].isMorningIntact() || states[s2].isMorningIntact())
+					|| v == R[1] && (states[s1].isNoonIntact() || states[s2].isNoonIntact())
+					|| v == R[2] && (states[s1].isAfternoonIntact() || states[s2].isAfternoonIntact())
+					|| v == R[3] && (states[s1].isEveningIntact() || states[s2].isEveningIntact())){
 				
-		for(IntVar var: vars)
-			if(!var.isInstantiated())
-				return var;
-		return null;
+				//good
+			} else {
+				System.out.println("labas");
+				
+			}
+		} else {
+			mini = 0;
+		}
 	}
 }
